9ad2533874fd8c1b8f4d1d225cad962658168533
Anthony Federico <anfederico@users.noreply.github.com>
2020-04-11 11:53:06 -0400

    Make smaller

--- data-raw/init.R
+++ data-raw/init.R
@@ -1,10 +1,27 @@
 library(usethis)
 
-#' An expression set object for a subset of TCGA-BRCA RNA-Seq data
-#' Processed with STAR2/HTSeq and downloaded via the GDC
-#' DESeq2-log2-normalized
-#' Contains molecular subtypings
-#' Top 2500 variable genes by median absolute deviation
+# TCGA-BRCA RNA-Seq data
 rnaseq <- readRDS(Sys.getenv("TCGA-BRCA"))
-rnaseq <- rnaseq[names(sort(apply(exprs(rnaseq), 1, mad), decreasing=TRUE)[1:2500]),]
+rnaseq <- rnaseq[,!is.na(rnaseq$subtype_m_rna)]
+
+# Top varying genes by median absolute deviation
+rnaseq <- rnaseq[names(sort(apply(Biobase::exprs(rnaseq), 1, mad), decreasing=TRUE)[1:1000]),]
+
+# Representative samples of the data
+pick.samples <- function(eset, subtype, top=50) {
+    eset <- eset[,eset$subtype_m_rna == subtype]
+    pca <- prcomp(Biobase::exprs(eset), scale=TRUE)
+    pca.summary <- summary(pca)
+    pc1 <- pca$x[,"PC1"]
+    pc1.cor <- apply(Biobase::exprs(eset), 2, function(x) abs(cor(x, pc1)))
+    names(sort(pc1.cor, decreasing=TRUE))[1:top]
+}
+
+samples <- c(pick.samples(rnaseq, subtype="LumA"),
+             pick.samples(rnaseq, subtype="LumB"),
+             pick.samples(rnaseq, subtype="Her2"),
+             pick.samples(rnaseq, subtype="Basal"),
+             pick.samples(rnaseq, subtype= "Normal"))
+
+rnaseq <- rnaseq[,samples]
 usethis::use_data(rnaseq)
